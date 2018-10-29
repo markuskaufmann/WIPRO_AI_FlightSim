6487f945cf09726f8b09c3e4ffbeb508d853a1f0
Cyrille <cyrille.ulmi@stud.hslu.ch>
2018-10-29 11:06:43 +0100

    space definer updated

--- FG_Connect_Example/ch/hslu/wipro/ddpg/utility/SpaceDefiner.py
+++ FG_Connect_Example/ch/hslu/wipro/ddpg/utility/SpaceDefiner.py
@@ -2,6 +2,7 @@ from ch.hslu.wipro.ddpg.spaces.Box import Box
 import numpy as np
 
 _normal_box = Box(low=-1, high=1, shape=(1,), dtype=np.float32)
+_positive_box = Box(low=0, high=1, shape=(1,), dtype=np.float32)
 
 DefaultActionSpaces = [
     (True, 'throttle', _normal_box),
@@ -10,12 +11,16 @@ DefaultActionSpaces = [
 ]
 
 DefaultObservationSpaces = [
-    (True, 'throttle', _normal_box),
-    (True, 'mixture', _normal_box),
+    (True, 'throttle', _positive_box),
+    (True, 'mixture', _positive_box),
     (True, 'aileron', _normal_box),
     (True, 'elevator', _normal_box),
     (True, 'rudder', _normal_box),
-    (True, 'flaps', _normal_box)
+    (True, 'flaps', _positive_box),
+    (True, 'alt_m', _normal_box),
+    (True, 'dist_m', _normal_box),
+    (True, 'discrepancy', _normal_box),
+    (True, 'bearing_deg', _normal_box)
 ]
 
 DefaultObservationSpaceKeys = ['throttle', 'mixture', 'aileron', 'elevator', 'rudder', 'flaps']
